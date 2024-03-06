9afb9baf4d1643300a638aa89cafd53d36a9635b
kimbap0213 <35618313+kimbap0213@users.noreply.github.com>
2024-03-06 21:25:07 +0900

    feat : 우선순위 큐 추가

--- Assets/Scripts/MouseInteraction.cs
+++ Assets/Scripts/MouseInteraction.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class MouseInteraction : MonoBehaviour
@@ -37,6 +38,11 @@ public class MouseInteraction : MonoBehaviour
         GameObject go = new GameObject("MouseInteraction");
         _instance = go.AddComponent<MouseInteraction>();
         _instance._camera = Camera.main;
+        
+        PriorityQueue<int> queue = new (Comparer<int>.Create((x, y) =>
+        {
+            return x.CompareTo(y);
+        }));
     }
     
     void Update()
@@ -133,9 +139,92 @@ public class MouseInteraction : MonoBehaviour
         if (node.Parent != null)
         {
             Debug.Log(node.Position + " " + node.Parent.Position + " " + dest);
-            //Debug.DrawRay(node.Position, node.Position - node.Parent.Position, Color.green, 2);
             Debug.DrawLine(node.Position, node.Parent.Position, Color.red, 2);
             Draw(node.Parent, dest);
         }
     }
+}
+
+public class PriorityQueue<T>
+{
+    private List<T> _data;
+    private IComparer<T> _comparer;
+    public T Peek() => _data[0];
+    public int Count => _data.Count;
+    
+    public PriorityQueue(IComparer<T> comparer)
+    {
+        _data = new List<T>();
+        _comparer = comparer;
+    }
+    
+    public void Enqueue(T item)
+    {
+        _data.Add(item);
+        EnqueueSort(_data.Count);
+    }
+    
+    private void EnqueueSort(int idx)
+    {
+        if (idx <= 1)
+            return;
+        
+        int compare = _comparer.Compare(GetValueAt(idx), GetValueAt(idx / 2));
+
+        if (compare <= 0)
+            return;
+        
+        Swap(idx, idx / 2);
+        
+        EnqueueSort(idx / 2);
+    }
+    
+    public T Dequeue()
+    {
+        T item = _data[0];
+        
+        _data[0] = _data[_data.Count - 1];
+        
+        _data.RemoveAt(_data.Count - 1);
+        
+        DequeueSort(1);
+        
+        return item;
+    }
+    
+    private void DequeueSort(int idx)
+    {
+        if (idx * 2 > _data.Count)
+            return;
+        
+        if(idx * 2 + 1 > _data.Count)
+        {
+            int compare = _comparer.Compare(GetValueAt(idx), GetValueAt(idx * 2));
+            if (compare < 0)
+                Swap(idx, idx * 2);
+            return;
+        }
+        
+        int compare1 = _comparer.Compare(GetValueAt(idx), GetValueAt(idx * 2));
+        int compare2 = _comparer.Compare(GetValueAt(idx), GetValueAt(idx * 2 + 1));
+        
+        if (compare1 > 0 && compare2 > 0)
+            return;
+        
+        int swapCompare = _comparer.Compare(GetValueAt(idx * 2), GetValueAt(idx * 2 + 1));
+        
+        int swapIdx = swapCompare > 0 ? idx * 2 : idx * 2 + 1;
+        
+        Swap(idx, swapIdx);
+        
+        DequeueSort(swapIdx);
+    }
+    
+    private void Swap(int idx1, int idx2)
+    {
+        (_data[idx1 - 1], _data[idx2 - 1]) = (_data[idx2 - 1], _data[idx1 - 1]);
+    }
+    
+    private T GetValueAt(int idx) => _data[idx - 1];
+    
 }
